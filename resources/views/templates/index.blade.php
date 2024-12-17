2fb06e986290b681ccd8e86765b449a135526541
ykrszchkm <soyyakisir@gmail.com>
2024-12-17 15:50:01 +0100

    intégration du template

---
+++ resources/views/templates/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+
+<head>
+    @include('templates.partials._head')
+</head>
+
+
+<body class="bg-gray-100 text-gray-800 font-sans leading-normal tracking-normal">
+
+
+    @include('templates.partials._header')
+
+
+    @include('templates.partials._main')
+
+
+    @include('templates.partials._footer')
+
+
+</body>
+
+
+</html>
---
+++ resources/views/templates/partials/_main.blade.php
@@ -0,0 +1,11 @@
+<div class="container mx-auto flex flex-wrap pt-4 pb-12 text-gray-800">
+
+
+    @include('templates.partials._aside')
+
+
+    <!-- Main content -->
+    <main class="w-full md:w-3/4 p-3">
+        @yield('content')
+    </main>
+</div>
